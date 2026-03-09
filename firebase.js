fa08d0dbf8e10d41abcd8219d977ad6718d0d9d7
appbaha88-netizen <appbaha88@gmail.com>
2026-03-09 21:38:07 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth, signInWithPhoneNumber, RecaptchaVerifier, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth, signInWithPhoneNumber, RecaptchaVerifier, signInWithRedirect, getRedirectResult, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBp1XpwzR2-FlYBWgdA5oQLaFt8_f4uqFE",
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, auth, collection, addDoc, getDocs, deleteDoc, doc, setDoc, signInWithPhoneNumber, RecaptchaVerifier, signInWithPopup, GoogleAuthProvider, onAuthStateChanged };
+export { db, auth, collection, addDoc, getDocs, deleteDoc, doc, setDoc, signInWithPhoneNumber, RecaptchaVerifier, signInWithRedirect, getRedirectResult, GoogleAuthProvider, onAuthStateChanged };
